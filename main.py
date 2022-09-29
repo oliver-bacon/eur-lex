8ea98f12e65a66fa64a0215fbf4fcad4cf07cea7
Oliver Bacon <thespine1337@gmail.com>
2022-09-29 20:00:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import xml.etree.ElementTree as ET
 
 cellar = "http://publications.europa.eu/resource/cellar/fe93672d-fd84-414b-82ef-68da9cb805e3.0003.04/DOC_1"
-celex = "http://publications.europa.eu/resource/celex/12012E"
+celex = "http://publications.europa.eu/resource/celex/12012E.ENG.print"
 oj = "http://publications.europa.eu/resource/oj/JOC_2012_326_R.ENG.fmx4.C_2012326EN.01000101.xml"
 headers = {"Accept": "application/xml;notice=object",
            "Accept-Language": "eng"}
@@ -19,5 +19,3 @@ with open("response.txt", "w") as file:
     file.write(data)
 
 print(data)
-
-
