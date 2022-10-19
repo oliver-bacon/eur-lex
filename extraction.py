acda0583cbbaefb7521eeb69fd0e99c21f471c03
Alexander Ulmer <alexander.j.ulmer@gmail.com>
2022-10-19 11:34:09 +0200

    Review

--- extraction.py
+++ extraction.py
@@ -3,22 +3,32 @@ import zipfile
 
 
 class Extractor:
-
-    def __init__(self, celex_number):
+    def __init__(self, celex_number, language="eng"):
         self.celex_number = celex_number
         self.uri = f"http://publications.europa.eu/resource/celex/{celex_number}"
-        self.headers = {"Accept": "application/zip;mtype=fmx4",
-                        "Accept-Language": "eng"}
+        self.headers = {
+            "Accept": "application/zip;mtype=fmx4",
+            # [aju] It's a minor thing, but you could make the language a parameter
+            # [aju] of the class, so that you can easily switch between languages.
+            # [aju] You can then set the default to "eng" so that users only have
+            # [aju] to specify the language if they want another language than English.
+            "Accept-Language": language,
+        }
 
     def get_zip(self):
         response = requests.get(self.uri, headers=self.headers)
+        # [aju] If you raise for status, you could add some error handling
         response.raise_for_status()
 
+        # [aju] See my commend in main.py
         with open("files/response.zip", mode="wb") as file:
             file.write(response.content)
 
+        # [aju] If you look at the lexparancy implementation, you can find a nice
+        # [aju] use of a dictionary with the filenames as keys and ET.Elements as values.
+        # [aju] This dictionary is then easy to pass around.
         with zipfile.ZipFile("files/response.zip") as file:
             namelist = file.namelist()
             for name in namelist:
-                if not name.endswith('doc.xml'):
+                if not name.endswith("doc.xml"):
                     file.extract(name, path="files/xml")
--- main.py
+++ main.py
@@ -6,12 +6,23 @@ import pprint
 celex_number = "32008R0593"
 
 extc = Extractor(celex_number)
+
+# [aju] You could return the entire ZIP file, then you would not need to write
+# [aju] it to disk. Writing to disk is comparatively slow.
 extc.get_zip()
 
+# [aju] This is a nice touch! But you actually also need the .doc.xml file,
+# [aju] because it contains important information about the structure of the zip file.
 filename = input(f"Files:\n{listdir('./files/xml')}\nPlease choose a file:\n")
+
+# [aju] Here you could pass the ZIP file object rather than loading from disk
 frmx = Formex(filename)
 
-with open("files/dict.txt", "w") as file:
+# [aju] If you write the Python dict to disk, you need to use a .py file.
+# [aju] It would be better though to use a JSON file, because it is more
+# [aju] usable in other systems.
+# [aju] You could also add the functionality to the Fromex class.
+with open("files/dict.json", "w") as file:
     file.write(pprint.pformat(frmx.to_dict()))
 
 # print(frmx.to_dict()["chapters"][2]["articles"][1]["paragraphs"][0]["p_text"])
